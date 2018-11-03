0b9d8ec22a0d0274089a84c5bb72fc4cab947c1d
Kishen N Gowda <kishen_19@yahoo.com>
2018-11-04 03:57:56 +0530

    Another update

--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class hospital(models.Model):
     name = models.CharField(max_length=250)
-    locationx = models.FloatField()
-    locationy = models.FloatField()
+    x = models.FloatField()
+    y = models.FloatField()
     address = models.CharField(max_length=1000)
+    status = models.BooleanField()
+    supplyrequest = models.CharField(max_length=5000)
--- main/views.py
+++ main/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render,HttpResponse
-
+from django.http import JsonResponse
+from math import sqrt
 from .models import hospital
+import mpu
+
+def dist(x1, y1, x2, y2):
+    return mpu.haversine_distance((x1, y1), (x2, y2))
 
 
-
-def index(request):
-    return render(request, 'main.html', {})
+def sort(x, y, l):
+    dis = []
+    for i in l:
+        dis.append[[dist(x, y, i.locationx, i.locationy), i.name, i.locationx, i.locationy, i.address, i.status]]
+    dis.sort()
+    final = []
+    for j in dis:
+        d = {}
+        d['name'] = j[1]
+        d['lati'] = j[2]
+        d['longi'] = j[3]
+        d['addr'] = j[4]
+        d['status'] = j[5]
+        final.append(d)
+    return final
 
 
 def handle(request):
-    locationx = 0
-    locationy = 0
+    x = request.POST.get('lati')
+    y = request.POST.get('longi')
     hospitals = hospital.objects.all()
-
-    print(hospitals)
+    return JsonResponse(sort(x, y, hospitals))
