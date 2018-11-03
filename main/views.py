4f4b93e4d2466ad82a6c67c63ae4e828b534e324
Kishen N Gowda <kishen_19@yahoo.com>
2018-11-04 00:39:29 +0530

    Initial Commit

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from .models import hospital
+
+
+
+def index(request):
+    return render(request, 'main.html', {})
+
+
+def handle(request):
+    locationx = 0
+    locationy = 0
+    hospitals = hospital.objects.all()
+    print(hospitals)
